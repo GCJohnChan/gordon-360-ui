51efba43a8f7746e61b37a312c8ca04ea9d9d329
John Chan <John.Chan@gordon.edu>
2018-04-28 18:48:50 -0400

    Replaced index. js for eventual concat

--- src/views/Profile/Components/DialogApp/index.js
+++ src/views/Profile/Components/DialogApp/index.js
@@ -5,63 +5,20 @@ import Dialog, {
   DialogContentText,
   DialogTitle,
 } from 'material-ui/Dialog';
-//import FlatButton from 'material-ui/FlatButton';
 import Button from 'material-ui/Button';
-//import RaisedButton from 'material-ui/RaisedButton';
-import ButtonApp from './ButtonApp';
-import TimePickerApp from './TimePickerApp';
-import CheckboxApp1 from './CheckboxApp1';
-import Entries from './Entries';
 
-/**
- * Dialog with action buttons. The actions are passed in as an array of React objects,
- * in this example [FlatButtons](/#/components/flat-button).
- *
- * You can also close this dialog by clicking outside the dialog, or with the 'Esc' key.
- */
 class DialogApp extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { date: null, day: null, entries: [] };
-    this.onDateChanged = this.onDateChanged.bind(this);
   }
 
-  onDateChanged = date => {
-    this.setState({
-      date,
-    });
-  };
-
-  onAddClick = () => {
-    let entries = this.state.entries;
-    if (this.state.date && this.state.day) {
-      entries.push({ date: this.state.date, day: this.state.day });
-      this.setState({
-        entries,
-      });
-    } else {
-      alert('Please select a date and day');
-    }
-  };
-
-  onDeleteClick = () => {
-    let entries = this.state.entries;
-    entries.pop();
-    this.setState({
-      entries,
-    });
-  };
-
-  onDayChange = day => {
-    this.setState({
-      day,
-    });
-  };
-
+  //Concatinating code Step 1: Add Dialouge Handlers
+  //Set DialogBox intial state
   state = {
     open: false,
   };
 
+  //DialogBox Handelers
   handleOpen = () => {
     this.setState({ open: true });
   };
@@ -85,18 +42,7 @@ class DialogApp extends React.Component {
           onClose={this.handleClose}
           arialabelledby="form-dialog-title"
         >
-          <DialogContent>
-            <Entries entries={this.state.entries} />
-            <ButtonApp onAddClick={this.onAddClick} onDeleteClick={this.onDeleteClick} />
-            <tr>
-              <td>
-                <CheckboxApp1 onChange={this.onDayChange} />
-              </td>
-              <td>
-                <TimePickerApp onChange={this.onDateChanged} />
-              </td>
-            </tr>
-          </DialogContent>
+          <DialogContent />
           <DialogActions>
             <Button onClick={this.handleClose} color="primary">
               Cancel
---
+++ src/views/Profile/Components/DialogApp/oldindex.js
@@ -0,0 +1,116 @@
+import React from 'react';
+import Dialog, {
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from 'material-ui/Dialog';
+import Button from 'material-ui/Button';
+
+import ButtonApp from './ButtonApp';
+
+import TimePickerApp from './TimePickerApp';
+
+import CheckboxApp1 from './CheckboxApp1';
+
+import Entries from './Entries';
+
+/**
+ * Dialog with action buttons. The actions are passed in as an array of React objects,
+ * in this example [FlatButtons](/#/components/flat-button).
+ *
+ * You can also close this dialog by clicking outside the dialog, or with the 'Esc' key.
+ */
+class DialogApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { date: null, day: null, entries: [] };
+    this.onDateChanged = this.onDateChanged.bind(this);
+  }
+
+  onDateChanged = date => {
+    this.setState({
+      date,
+    });
+  };
+
+  onAddClick = () => {
+    let entries = this.state.entries;
+    if (this.state.date && this.state.day) {
+      entries.push({ date: this.state.date, day: this.state.day });
+      this.setState({
+        entries,
+      });
+    } else {
+      alert('Please select a date and day');
+    }
+  };
+
+  onDeleteClick = () => {
+    let entries = this.state.entries;
+    entries.pop();
+    this.setState({
+      entries,
+    });
+  };
+
+  onDayChange = day => {
+    this.setState({
+      day,
+    });
+  };
+
+  state = {
+    open: false,
+  };
+
+  handleOpen = () => {
+    this.setState({ open: true });
+  };
+
+  handleClose = () => {
+    this.setState({ open: false });
+  };
+
+  handlePrint = () => {
+    this.setState({ open: false });
+  };
+
+  render() {
+    return (
+      <div>
+        <Button variant="raised" onClick={this.handleOpen}>
+          DialogActions
+        </Button>
+        <Dialog
+          open={this.state.open}
+          onClose={this.handleClose}
+          arialabelledby="form-dialog-title"
+        >
+          <DialogContent>
+            <Entries entries={this.state.entries} />
+            <ButtonApp onAddClick={this.onAddClick} onDeleteClick={this.onDeleteClick} />
+            <tr>
+              <td>
+                <CheckboxApp1 onChange={this.onDayChange} />
+              </td>
+              <td>
+                <TimePickerApp onChange={this.onDateChanged} />
+              </td>
+            </tr>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={this.handleClose} color="primary">
+              Cancel
+            </Button>
+            <Button onClick={this.handleClose} color="primary" autoFocus>
+              Submit
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </div>
+    );
+  }
+}
+
+export default DialogApp;
