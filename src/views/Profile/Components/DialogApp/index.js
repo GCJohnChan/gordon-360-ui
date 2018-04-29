2be4e96b0351cdaaeb03e055049708c5f8616111
John Chan <John.Chan@gordon.edu>
2018-04-28 21:39:06 -0400

    Added Timepicker

--- src/views/Profile/Components/DialogApp/index.js
+++ src/views/Profile/Components/DialogApp/index.js
@@ -7,18 +7,21 @@ import Dialog, {
 } from 'material-ui/Dialog';
 import Button from 'material-ui/Button';
 import {
-    FormLabel,
-    FormControl,
-    FormGroup,
-    FormControlLabel,
-    FormHelperText,
-  } from 'material-ui/Form';
-  import Checkbox from 'material-ui/Checkbox';
+  FormLabel,
+  FormControl,
+  FormGroup,
+  FormControlLabel,
+  FormHelperText,
+} from 'material-ui/Form';
+import Checkbox from 'material-ui/Checkbox';
+import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui/styles';
+import TextField from 'material-ui/TextField';
 
 class DialogApp extends React.Component {
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
 
   //Concatinating code Step 1: Add Dialouge Handlers
   //Set DialogBox,checkbox intial state
@@ -29,7 +32,7 @@ class DialogApp extends React.Component {
     checkWeds: false,
     checkThurs: false,
     checkFri: false,
-    startt:'00:00',
+    startt: '00:00',
     endt: '00:00',
   };
 
@@ -51,90 +54,119 @@ class DialogApp extends React.Component {
     this.setState({ [name]: event.target.checked });
   };
 
+  //TimePicker Handler. Pretty much same as checkbox, just pulling out value
+  handleTime = name => event => {
+    this.setState({ [name]: event.target.value });
+  };
   render() {
     //Allows dynamic components (namely checkbox) to access states?
     const { classes } = this.props;
     return (
       <div>
         <Button variant="raised" onClick={this.handleOpen}>
-            DialogActions
+          DialogActions
         </Button>
         <Dialog
           open={this.state.open}
           onClose={this.handleClose}
           arialabelledby="form-dialog-title"
         >
-            <DialogContent>
-                <tr>
-                    <td>
-                        <FormControl component="fieldset">
-                            <FormGroup>
-                                <FormControlLabel
-                                    control={
-                                        <Checkbox
-                                            checked={this.state.checkMon}
-                                            onChange={this.handleCheck('checkMon')}
-                                            value="checkMon"
-                                        />
-                                    }
-                                    label="Monday"
-                                />
-                                <FormControlLabel
-                                    control={
-                                        <Checkbox
-                                            checked={this.state.checkTues}
-                                            onChange={this.handleCheck('checkTues')}
-                                            value="checkTues"
-                                        />
-                                    }
-                                    label="Tuesday"
-                                />
-                                <FormControlLabel
-                                    control={
-                                        <Checkbox
-                                            checked={this.state.checkWeds}
-                                            onChange={this.handleCheck('checkWeds')}
-                                            value="checkWeds"
-                                        />
-                                    }
-                                    label="Wednesday"
-                                />
-                                <FormControlLabel
-                                    control={
-                                        <Checkbox
-                                            checked={this.state.checkThurs}
-                                            onChange={this.handleCheck('checkThurs')}
-                                            value="checkThurs"
-                                        />
-                                    }
-                                    label="Thursday"
-                                />
-                                <FormControlLabel
-                                    control={
-                                        <Checkbox
-                                            checked={this.state.checkFri}
-                                            onChange={this.handleCheck('checkFri')}
-                                            value="checkFri"
-                                        />
-                                    }
-                                    label="Friday"
-                                />
-                            </FormGroup>
-                        </FormControl>
-                    </td>
-                    <td>
-
-                    </td>
-                </tr>
-            </DialogContent>
-            <DialogActions>
-                <Button onClick={this.handleClose} color="primary">
-                    Cancel
-                </Button>
-                <Button onClick={this.handleClose} color="primary" autoFocus>
-                    Submit
-                </Button>
-            </DialogActions>
+          <DialogContent>
+            <tr>
+              <td>
+                <FormControl component="fieldset">
+                  <FormGroup>
+                    <FormControlLabel
+                      control={
+                        <Checkbox
+                          checked={this.state.checkMon}
+                          onChange={this.handleCheck('checkMon')}
+                          value="checkMon"
+                        />
+                      }
+                      label="Monday"
+                    />
+                    <FormControlLabel
+                      control={
+                        <Checkbox
+                          checked={this.state.checkTues}
+                          onChange={this.handleCheck('checkTues')}
+                          value="checkTues"
+                        />
+                      }
+                      label="Tuesday"
+                    />
+                    <FormControlLabel
+                      control={
+                        <Checkbox
+                          checked={this.state.checkWeds}
+                          onChange={this.handleCheck('checkWeds')}
+                          value="checkWeds"
+                        />
+                      }
+                      label="Wednesday"
+                    />
+                    <FormControlLabel
+                      control={
+                        <Checkbox
+                          checked={this.state.checkThurs}
+                          onChange={this.handleCheck('checkThurs')}
+                          value="checkThurs"
+                        />
+                      }
+                      label="Thursday"
+                    />
+                    <FormControlLabel
+                      control={
+                        <Checkbox
+                          checked={this.state.checkFri}
+                          onChange={this.handleCheck('checkFri')}
+                          value="checkFri"
+                        />
+                      }
+                      label="Friday"
+                    />
+                  </FormGroup>
+                </FormControl>
+              </td>
+              <td>
+                <form noValidate>
+                  <TextField
+                    id="startt"
+                    label="Start Time"
+                    type="time"
+                    onChange={this.handleTime('startt')}
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+                    inputProps={{
+                      step: 300, // 5 min
+                    }}
+                  />
+                  <TextField
+                    id="endt"
+                    label="End Time"
+                    type="time"
+                    onChange={this.handleTime('endt')}
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+                    inputProps={{
+                      step: 300, // 5 min
+                    }}
+                  />
+                </form>
+              </td>
+            </tr>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={this.handleClose} color="primary">
+              Cancel
+            </Button>
+            <Button onClick={this.handleClose} color="primary" autoFocus>
+              Submit
+            </Button>
+          </DialogActions>
         </Dialog>
       </div>
     );
